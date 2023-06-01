7feea8e99e459798add9a91a3d60edec072e3df2
nick <just_9@hotmail.com>
2023-06-01 12:37:37 +0200

    First commit in Java API project

---
+++ calculatorAPI/src/main/java/nickTest/calculatorAPI/App.java
@@ -0,0 +1,13 @@
+package nickTest.calculatorAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
