ff61040d5801583bf7799544ebc771d598af419e
nick <just_9@hotmail.com>
2023-06-15 08:09:43 +0200

    Multiplication function is added in Calculator API

--- calculatorAPI/src/main/java/nickTest/calculatorAPI/CalculatorVO.java
+++ calculatorAPI/src/main/java/nickTest/calculatorAPI/CalculatorVO.java
@@ -3,6 +3,7 @@ package nickTest.calculatorAPI;
 public class CalculatorVO {
 	private int firstNumber;
 	private int secondNumber;
+	private int constantNumber = 100;
 	
 	public int getFirstNumber() {
 		return firstNumber;
@@ -15,5 +16,11 @@ public class CalculatorVO {
 	}
 	public void setSecondNumber(int secondNumber) {
 		this.secondNumber = secondNumber;
+	}
+	public int getConstant() {
+		return constantNumber;
+	}
+	public void setConstant(int constantNumber) {
+		this.constantNumber = 100;
 	}
 }
